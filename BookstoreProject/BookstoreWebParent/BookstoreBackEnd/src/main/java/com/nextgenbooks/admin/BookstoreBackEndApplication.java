a873eb6129cff520ba0f87f1133280404aa77975
Yen Le <yenhgle@gmail.com>
2022-05-03 03:10:40 -0700

    init commit

---
+++ BookstoreProject/BookstoreWebParent/BookstoreBackEnd/src/main/java/com/nextgenbooks/admin/BookstoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.nextgenbooks.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreBackEndApplication.class, args);
+	}
+
+}
---
+++ BookstoreProject/BookstoreWebParent/BookstoreFrontEnd/src/main/java/com/nextgenbooks/site/BookstoreFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.nextgenbooks.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreFrontEndApplication.class, args);
+	}
+
+}
